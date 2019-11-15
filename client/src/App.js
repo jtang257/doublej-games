5c83b715e11ca86a3f3ef766732f36a457513383
John Heckbert <johnheckbert@yahoo.ca>
2019-11-15 11:54:11 -0500

    data load ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+// build the react server
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+
+// build the app components
 import BingoCard from './components/BingoCard/BingoCard.js';
 import BingoDisplay from './components/BingoDisplay/BingoDisplay.js';
 import Landing from './components/Landing/Landing.js';
 
+// api call 
+axios
+  .get('http://localhost:7700/getMyCard')
+    .then(result => {
+      console.log(result);  
+    })
 class App extends React.Component {
   render() {
     return(

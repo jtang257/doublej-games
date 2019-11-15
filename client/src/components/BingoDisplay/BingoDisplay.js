78fc3d5ca553d731904566599f6c02c6da181fdc
Jonathan Tang <jonathantang.525@gmail.com>
2019-11-15 10:22:55 -0500

    morning refresh

--- client/src/components/BingoDisplay/BingoDisplay.js
+++ client/src/components/BingoDisplay/BingoDisplay.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import Header from '../Header/Header';
+import presenter from '../../assets/presenter.png';
 
 class BingoDisplay extends React.Component {
 
     render() {
         return(
+            <>
             <Header />
-
+            <img src={presenter} />
+            </>
         )
     }
 }

7b98ecd15ba67975a5742becb3e706976d3c96a5
Jonathan Tang <jonathantang.525@gmail.com>
2019-11-15 10:37:19 -0500

    worked on display

--- client/src/components/BingoDisplay/BingoDisplay.js
+++ client/src/components/BingoDisplay/BingoDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Header from '../Header/Header';
+import './bingoDisplay.scss';
 import presenter from '../../assets/presenter.png';
 
 class BingoDisplay extends React.Component {
@@ -9,7 +10,16 @@ class BingoDisplay extends React.Component {
         return(
             <>
             <Header />
-            <img src={presenter} />
+            <div className="display">
+                <div className="display__container">
+                    <img className="display__image"src={presenter} />
+                    <div className="display__words">Words</div>
+                    <div className="display__words">Words</div>
+                    <div className="display__words">Words</div>
+                    <div className="display__words">Words</div>
+                    <div className="display__words">Words</div>
+                </div>
+            </div>
             </>
         )
     }

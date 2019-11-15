690b2b1d1da89d0a9f0909a5e30917559a9de639
Jonathan Tang <jonathantang.525@gmail.com>
2019-11-15 12:02:10 -0500

    Updated display UI

--- client/src/components/BingoDisplay/BingoDisplay.js
+++ client/src/components/BingoDisplay/BingoDisplay.js
@@ -3,21 +3,41 @@ import axios from 'axios';
 import Header from '../Header/Header';
 import './bingoDisplay.scss';
 import presenter from '../../assets/presenter.png';
+import bingoSpinner from '../../assets/bingo-spinner.png';
+import displayScreen from '../../assets/display-screen.png';
 
 class BingoDisplay extends React.Component {
 
+
+
+    displayWord = (event) => {
+        let content = event;
+        console.log(event);
+    }
+
     render() {
+
         return(
             <>
             <Header />
             <div className="display">
                 <div className="display__container">
-                    <img className="display__image"src={presenter} />
-                    <div className="display__words">Words</div>
-                    <div className="display__words">Words</div>
-                    <div className="display__words">Words</div>
-                    <div className="display__words">Words</div>
-                    <div className="display__words">Words</div>
+                    <div className="display__image-container">
+                        <img className="display__image"src={bingoSpinner} onClick={null} />
+                    </div>
+                    <div className="display__words-container">
+                        <div className="display__words" onClick={(e) => {this.displayWord(e)}}>☕ {this.props.words}</div>
+                        <div className="display__words" onClick={(e) => {this.displayWord(e)}}>☕ {this.props.words}</div>
+                        <div className="display__words" onClick={(e) => {this.displayWord(e)}}>☕ {this.props.words}</div>
+                        <div className="display__words" onClick={(e) => {this.displayWord(e)}}>☕ {this.props.words}</div>
+                        <div className="display__words" onClick={(e) => {this.displayWord(e)}}>☕ {this.props.words}</div>
+                    </div>
+                </div>
+                <div className="display__presenter">
+                    <img className="display__presenter-image" src={displayScreen} />
+                    <div className="display__presenter-word-container">
+                        <p className="display__presenter-word" ref="word">WORD MORE WORDS MORE WORDS</p>
+                    </div>
                 </div>
             </div>
             </>
